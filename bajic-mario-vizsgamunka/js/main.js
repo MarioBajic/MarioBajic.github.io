a6ca89d47d09d5cc0f3045701b3a57e027103da3
“MarioBajic” <mariobajic@gmail.com>
2025-08-16 22:31:23 +0200

    elso feltoltes

---
+++ bajic-mario-vizsgamunka/js/main.js
@@ -0,0 +1,61 @@
+function toggleMenu(button) {
+  const menu = document.querySelector("ul.list-menu-inline");
+  menu.classList.toggle("active");
+  // hamburger animáció
+  button.classList.toggle("active");
+}
+
+
+//Scroll to top button
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollBtn = document.getElementById("scrollToTopBtn");
+
+  if (scrollBtn) {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+        scrollBtn.classList.add("show");
+      } else {
+        scrollBtn.classList.remove("show");
+      }
+    });
+
+    scrollBtn.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  }
+});
+
+
+//visszajelzés
+document.addEventListener("DOMContentLoaded", function () {
+    const forms = document.querySelectorAll("form");
+
+    forms.forEach(form => {
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            // formon belül keressük a success-message-et
+            const successMessage = form.querySelector(".success-message");
+            if (successMessage) {
+                successMessage.style.display = "block";
+
+                setTimeout(() => {
+                    successMessage.style.display = "none";
+                }, 3000);
+            }
+
+            form.reset();
+        });
+    });
+});
+
+
+
+
+
+
+
+
